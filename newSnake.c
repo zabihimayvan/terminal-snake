94d97b9e507e8ca8efdcc50d09660fb648ccaa59
kennethkoch <kennykoch47@gmail.com>
2021-04-25 16:15:07 -0400

    fixed indentation in main

--- newSnake.c
+++ newSnake.c
@@ -7,20 +7,19 @@
 
 #define DELAY 60000 //Controls speed of snake
 
-/*a coordPair will contain an xCoord and a yCoord, which will represent the coordinates
+int maxY, maxX;
+int nextX, nextY; 
+int snakeLength = 5, inMotion = 1;
+char currentDir;
+
+/* a coordPair will contain an xCoord and a yCoord, which will represent the coordinates
 of the snake's body */
 typedef struct coordPair {
     int xCoord;
     int yCoord;
 } coordPair;
 
-int maxY, maxX;
-int nextX, nextY; 
-int snakeLength = 5, inMotion = 1;
-char currentDir;
-
-/* creates the snake as an array of coordinate pairs, each coordPair contains
-an xCoord and yCoord value*/
+/* creates the snake as an array of coordinate pairs */ 
 coordPair snakeBody[255] = {};
 
 //function to start the game
@@ -124,55 +123,53 @@ int main(int argc, char *argv[]) {
     while(inMotion) {
         
         //handle user input
-            int ch = getch();
-            /*only allow 90 degree changes in direction, no reversing*/
-            if(ch == KEY_RIGHT){
-                if (currentDir == 'L')
-                    inMotion = 0;
-                else
-                    currentDir = 'R';
-            } else if (ch == KEY_LEFT) {
-                if (currentDir == 'R')
-                    inMotion = 0;
-                currentDir = 'L';
-            } else if(ch == KEY_DOWN) {
-                if (currentDir == 'U')
-                    inMotion = 0;
-                currentDir = 'D';
-            } else if(ch == KEY_UP) {
-                if (currentDir == 'D')
-                    inMotion = 0;
-                currentDir = 'U';
-            }
-
-        //snake movement
-
-            //set nextX and nextY based on snakeBody[0]
-            nextX = snakeBody[0].xCoord;
-            nextY = snakeBody[0].yCoord;
-
-            //increment/decrement coordinates
-            if(currentDir == 'R') nextX++;
-            else if(currentDir == 'L') nextX--;
-            else if(currentDir == 'U') nextY--;
-            else if(currentDir == 'D') nextY++;
-
-            snakeBody[snakeLength - 1].xCoord = nextX;
-            snakeBody[snakeLength - 1].yCoord = nextY;
-
-            coordPair temp = snakeBody[snakeLength - 1];
-             for(int i = snakeLength - 1; i > 0; i--) {
-                snakeBody[i] = snakeBody[i-1];
-            }
-            snakeBody[0] = temp;
-
-            //end game if the snake hits the edges
-            if((nextX >= maxX || nextX < 0) || (nextY >= maxY || nextY < 0)) {
+        int ch = getch();
+        /*only allow 90 degree changes in direction, no reversing*/
+        if(ch == KEY_RIGHT){
+            if (currentDir == 'L')
                 inMotion = 0;
-            }
-
-            //draw screen, snake
-            draw();
+            else
+                currentDir = 'R';
+        } else if (ch == KEY_LEFT) {
+            if (currentDir == 'R')
+                inMotion = 0;
+            currentDir = 'L';
+        } else if(ch == KEY_DOWN) {
+            if (currentDir == 'U')
+                inMotion = 0;
+            currentDir = 'D';
+        } else if(ch == KEY_UP) {
+            if (currentDir == 'D')
+                inMotion = 0;
+            currentDir = 'U';
         }
+
+        //set nextX and nextY based on snakeBody[0]
+        nextX = snakeBody[0].xCoord;
+        nextY = snakeBody[0].yCoord;
+
+        //increment/decrement coordinates
+        if(currentDir == 'R') nextX++;
+        else if(currentDir == 'L') nextX--;
+        else if(currentDir == 'U') nextY--;
+        else if(currentDir == 'D') nextY++;
+
+        snakeBody[snakeLength - 1].xCoord = nextX;
+        snakeBody[snakeLength - 1].yCoord = nextY;
+
+        coordPair temp = snakeBody[snakeLength - 1];
+            for(int i = snakeLength - 1; i > 0; i--) {
+            snakeBody[i] = snakeBody[i-1];
+        }
+        snakeBody[0] = temp;
+
+        //end game if the snake hits the edges
+        if((nextX >= maxX || nextX < 0) || (nextY >= maxY || nextY < 0)) {
+            inMotion = 0;
+        }
+
+        //draw screen, snake
+        draw();
+    }    
 
 }
